b3be6d17cbb257788c91e112ea1bcc12414cac68
Daniel Donateli <danieldonateli@outlook.com>
2021-03-12 09:52:28 -0300

    Added scoring system

--- py_pong/components/window.py
+++ py_pong/components/window.py
@@ -1,22 +1,27 @@
-from pygame.draw import line
-import pygame.display as display
+import pygame
+from typing import Tuple, List
 from py_pong.components.constants import *
 
 class Window:
   def __init__(self):
     # Create surface
-    self.screen = display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    display.set_caption("Pong!")
+    self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Pong!")
   
-  def draw(self, sprite_groups) -> None:
+  def draw(self, sprite_groups: List[pygame.sprite.Sprite], scores: Tuple[int, int]) -> None:
     # Draw black background
     self.screen.fill(BLACK)
     # Draw net
-    line(self.screen, WHITE, [349, 0], [349, 500], 5)
+    pygame.draw.line(self.screen, WHITE, [349, 0], [349, 500], 5)
     # Draw sprites
     for sprite in sprite_groups:
       sprite.draw(self.screen)
+    # Display scores:
+    font = pygame.font.Font(None, 74)
+    text = font.render(str(scores[0]), 1, WHITE)
+    self.screen.blit(text, (250,10))
+    text = font.render(str(scores[1]), 1, WHITE)
+    self.screen.blit(text, (420,10))
   
   def update(self) -> None:
-    # Blit
-    display.flip()
+    pygame.display.flip()
--- py_pong/game.py
+++ py_pong/game.py
@@ -16,7 +16,7 @@ class Game:
     self.window = Window()
     self.player_1 = Player(rect_x = 20, rect_y = 200)
     self.player_2 = Player(rect_x = 670, rect_y = 200)
-    self.ball = Ball(color = WHITE, witdh = 10, height = 10)
+    self.ball = Ball(color = WHITE, width = 10, height = 10)
     self.ball.rect.x = 345
     self.ball.rect.y = 195
     self.clock = pygame.time.Clock()
@@ -43,8 +43,10 @@ class Game:
       
     # Check if the ball is bouncing against any of the 4 walls:
     if self.ball.rect.x >= 690:
+      self.player_1.score += 1
       self.ball.velocity[0] = -self.ball.velocity[0]
     if self.ball.rect.x <= 0:
+      self.player_2.score += 1
       self.ball.velocity[0] = -self.ball.velocity[0]
     if self.ball.rect.y > 490:
       self.ball.velocity[1] = -self.ball.velocity[1]
@@ -56,8 +58,8 @@ class Game:
       self.ball.bounce()
 
     # Draw sprites on surface
-    self.window.draw([self.all_sprites_list])
-    # Blit
+    self.window.draw([self.all_sprites_list], (self.player_1.score, self.player_2.score))
+
     self.window.update()
     
     self.clock.tick(60)
